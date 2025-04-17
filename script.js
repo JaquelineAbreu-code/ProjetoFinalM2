d2645afc35979e1bb989994f098177bc60514a3d
JaquelineAbreu-code <abreujaqueline61@gmail.com>
2025-04-17 18:58:11 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const questions = [
+    {
+        question: "1. Qual palavra-chave é usada para declarar uma variável em JavaScript?",
+        answers: [
+            { id: 1, text: "let", correct: true},
+            { id: 2, text: "varb", correct: false},
+            { id: 3, text: "def", correct: false},
+            { id: 4, text: "dim", correct: false},
+        ]
+    },
+]
+
+const questions = [
+    {
+        question: "2. Qual método é usado para exibir uma mensagem no console do navegador?",
+        answers: [
+            { id: 1, text: "diplay.log()", correct: false},
+            { id: 2, text: "console.print()", correct: false},
+            { id: 3, text: "console.log()", correct: true},
+            { id: 4, text: "show.log()", correct: false},
+        ]
+    },
+]
+
+
+const questions = [
+    {
+        question: "3. Qual método é usado para remover o último elemento de um array em JavaScript?",
+        answers: [
+            { id: 1, text: "shift()", correct: false},
+            { id: 2, text: "unshift()", correct: false},
+            { id: 3, text: "pop()", correct: true},
+            { id: 4, text: "splice()", correct: false},
+        ]
+    },
+]
+
+
+const questions = [
+    {
+        question: "1. Qual palavra-chave é usada para declarar uma variável em JavaScript?",
+        answers: [
+            { id: 1, text: "let", correct: true},
+            { id: 2, text: "varb", correct: false},
+            { id: 3, text: "def", correct: false},
+            { id: 4, text: "dim", correct: false},
+        ]
+    },
+]
+
+const questions = [
+    {
+        question: "1. Qual palavra-chave é usada para declarar uma variável em JavaScript?",
+        answers: [
+            { id: 1, text: "let", correct: true},
+            { id: 2, text: "varb", correct: false},
+            { id: 3, text: "def", correct: false},
+            { id: 4, text: "dim", correct: false},
+        ]
+    },
+]
+
+const questions = [
+    {
+        question: "1. Qual palavra-chave é usada para declarar uma variável em JavaScript?",
+        answers: [
+            { id: 1, text: "let", correct: true},
+            { id: 2, text: "varb", correct: false},
+            { id: 3, text: "def", correct: false},
+            { id: 4, text: "dim", correct: false},
+        ]
+    },
+]
