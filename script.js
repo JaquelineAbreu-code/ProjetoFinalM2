991a38b5eba83f3ad00fccf125ed128b76369f01
JaquelineAbreu-code <abreujaqueline61@gmail.com>
2025-04-17 21:11:53 -0300

    Funcionalidades

--- script.js
+++ script.js
@@ -59,3 +59,46 @@ const questions5 = [
         ]
     },
 ]
+
+
+const questionElement = document.getElementById("question");
+
+const answerButtons = document.getElementById("answer-buttons");
+
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Próxima";
+    showQuestion();
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+function showQuestion(){
+    resetState()
+    let currentQuestion = questions(currentQuestionIndex);
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "," + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answers => {
+        const button = document.createElement("button");
+        button.innerHTML = answers.text;
+        button.dataset.id = answers.id;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+    });
+
+}
+
+startQuiz();
